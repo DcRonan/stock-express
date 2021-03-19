0aae2c1f6e09ad0be741ac50c0aaab0271109145
Daniel Ronan <danielconnorronan@gmail.com>
2021-03-19 09:55:08 +0000

    create stock list container and install react router

---
+++ src/containers/StockList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const StockList = () => <div> Stock List</div>;
+
+export default StockList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
