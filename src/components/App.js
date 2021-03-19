8eb0be50245549344e2d5bab3db68c334808fe9d
Daniel Ronan <danielconnorronan@gmail.com>
2021-03-19 10:26:39 +0000

    add routes and nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Routes from '../routes';
 
-const App = () => <div> hi </div>;
+const App = () => (
+  <>
+    <nav>
+      <h1>
+        <NavLink to="/"> Home </NavLink>
+      </h1>
+    </nav>
+    <Routes />
+  </>
+);
 
 export default App;
