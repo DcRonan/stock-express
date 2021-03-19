d4ff818ea6c5e0da9c7cc89aa081ea8e306214b5
Daniel Ronan <danielconnorronan@gmail.com>
2021-03-19 15:34:16 +0000

    create StockList container and return working api request for data

--- src/containers/StockList.js
+++ src/containers/StockList.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { GET_STOCK_LIST } from '../actions/index';
 
-const StockList = () => <div> Stock List</div>;
+const StockList = () => {
+  const dispatch = useDispatch();
+  const stockList = useSelector(state => state.stockList);
+
+  const fetchData = () => {
+    dispatch(GET_STOCK_LIST(stockList.data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const showData = () => {
+    if (stockList.data !== undefined) {
+      return stockList.data.map(el => el.name);
+    }
+
+    if (stockList.loading) return <p> Loading... </p>;
+
+    if (stockList.errorMsg !== '') {
+      return <p>{stockList.errorMsg}</p>;
+    }
+
+    return <p> ERROR! - unable to get data </p>;
+  };
+
+  return <div>{showData()}</div>;
+};
 
 export default StockList;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import stockReducer from './stock';
+import stockListReducer from './stockList';
 
 const allReducers = combineReducers({
   stock: stockReducer,
+  stockList: stockListReducer,
 });
 
 export default allReducers;
