686ec93ce0ec9a3b94759e0d6a492841232087ca
Daniel Ronan <danielconnorronan@gmail.com>
2021-03-21 12:42:06 +0000

    add prop validation to Pagination and aff Link tag to replace a tag

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,6 @@
-/* eslint-disable */
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Pagination = ({ stocksPerPage, totalStocks, paginate }) => {
   const pageNumbers = [];
@@ -13,7 +14,7 @@ const Pagination = ({ stocksPerPage, totalStocks, paginate }) => {
       <ul>
         {pageNumbers.map(number => (
           <li key={number}>
-            <a onClick={() => paginate(number)} href="#hi">{number}</a>
+            <Link to={paginate(number)}>{number}</Link>
           </li>
         ))}
       </ul>
@@ -22,3 +23,15 @@ const Pagination = ({ stocksPerPage, totalStocks, paginate }) => {
 };
 
 export default Pagination;
+
+Pagination.defaultProps = {
+  stocksPerPage: 0,
+  totalStocks: 0,
+  paginate: () => {},
+};
+
+Pagination.propTypes = {
+  stocksPerPage: PropTypes.number,
+  totalStocks: PropTypes.number,
+  paginate: PropTypes.func,
+};
